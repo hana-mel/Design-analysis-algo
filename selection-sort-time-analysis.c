cab26fdfb1761d5fe5bdea85112fbd782c63bbc0
Hanamel Risaw <hrisaw16@gmail.com>
2026-04-14 15:10:49 +0530

    Refactor selection sort implementation and file handling

--- selection-sort-time-analysis.c
+++ selection-sort-time-analysis.c
@@ -1,111 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #define MAX 100
 #define MIN 1
 
-void selectionSort(int arr[], int n)
+void selectionSort(int a[],int n)
 {
-    int i, j, min_idx, temp;
-
-    for (i = 0; i < n - 1; i++)
+    int i,j,min,temp;
+    for(i=0;i<n-1;i++)
     {
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
+        min = i;
+        for(j=i+ 1;j<n;j++)
         {
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+            if(a[min] > a[j])
+                min = j;
         }
-        // Swap
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+        temp = a[min];
+        a[min] = a[i];
+        a[i] = temp;
     }
 }
 
 int main()
 {
     int sizes[10];
-    clock_t start, end;
-    double best, worst, avg;
-
-    FILE *fp;
-
-    // Open file for writing
-    fp = fopen("selection_sort_results.txt", "w");
-    if (fp == NULL)
+    clock_t start,end;
+    double best,avg,worst;
+    
+    srand(time(NULL));
+    for(int i =0;i<10;i++)
     {
-        printf("Error opening file!\n");
+        sizes[i] = rand() % (50000 - 50 + 1)+ 50;
+    }
+    FILE *fp = fopen("selection.txt","w");
+    if(fp == NULL)
+    {
+        printf("Error");
         return 1;
     }
-
-    srand(time(NULL));
-
-    // Generate 10 random sizes between 50 and 50000
-    for (int i = 0; i < 10; i++)
-    {
-        sizes[i] = rand() % (50000 - 50 + 1) + 50;
-    }
-
-    // Write header to file
-    fprintf(fp, "----------------------------------------------------------\n");
-    fprintf(fp, "%-12s %-16s %-16s %-16s\n", "Size", "Best(sec)", "Worst(sec)", "Avg(sec)");
-    fprintf(fp, "----------------------------------------------------------\n");
-    printf("----------------------------------------------------------\n");
-    printf("%-12s %-16s %-16s %-16s\n", "Size", "Best(sec)", "Worst(sec)", "Avg(sec)");
-    printf("----------------------------------------------------------\n");
-    for (int s = 0; s < 10; s++)
+    
+    printf("--------------------------------------------------\n");
+    printf("%-12s %-16s %-16s %-16s\n","Sizes","Best","Avg","Worst");
+    printf("--------------------------------------------------\n");
+    fprintf(fp,"--------------------------------------------------\n");
+    fprintf(fp,"%-12s %-16s %-16s %-16s\n","Sizes","Best","Avg","Worst");
+    fprintf(fp,"--------------------------------------------------\n");
+    
+    for(int s=0;s<10;s++)
     {
         int n = sizes[s];
-        int *arr = (int *)malloc(n * sizeof(int));
-
-        if (arr == NULL)
+        int *a = (int *)malloc(n * sizeof(int));
+        
+        if(a == NULL)
         {
-            printf("Memory allocation failed!\n");
+            printf("Memory allocation failed");
             fclose(fp);
             return 1;
         }
-
-        // Best Case
-        for (int i = 0; i < n; i++)
-            arr[i] = i + 1;
-
+        //Best
+        for(int i =0;i<n;i++)
+        {
+            a[i] = i+ 1;
+        }
         start = clock();
-        selectionSort(arr, n);
+        selectionSort(a,n);
         end = clock();
-        best = (double)(end - start) / CLOCKS_PER_SEC;
-
-        // Worst Case
-        for (int i = 0; i < n; i++)
-            arr[i] = n - i;
-
+        best = ((double)(end - start)) / CLOCKS_PER_SEC;
+        //Worst
+        for(int i =0;i<n;i++)
+        {
+            a[i] = n - i;
+        }
         start = clock();
-        selectionSort(arr, n);
+        selectionSort(a,n);
         end = clock();
-        worst = (double)(end - start) / CLOCKS_PER_SEC;
-
-        // Average Case
-        for (int i = 0; i < n; i++)
-            arr[i] = rand() % (MAX - MIN + 1) + MIN;
-
+        worst = ((double)(end - start)) / CLOCKS_PER_SEC;
+        //Avg
+        for(int i =0;i<n;i++)
+        {
+            a[i] = rand() % (MAX - MIN + 1)+ MIN;
+        }
         start = clock();
-        selectionSort(arr, n);
+        selectionSort(a,n);
         end = clock();
-        avg = (double)(end - start) / CLOCKS_PER_SEC;
-
-        // Write results to file
-        fprintf(fp, "%-12d %-16f %-16f %-16f\n", n, best, worst, avg);
-        printf("%-12d %-16f %-16f %-16f\n", n, best, worst, avg);
-
-        free(arr);
+        avg = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("%-12d %-16f %-16f %-16f\n",n,best,avg,worst);
+        fprintf(fp,"%-12d %-16f %-16f %-16f\n",n,best,avg,worst);
+        free(a);
+        
     }
-
-    fprintf(fp, "----------------------------------------------------------\n");
-    printf("----------------------------------------------------------\n");
-
+    printf("--------------------------------------------------\n");
+    fprintf(fp,"--------------------------------------------------\n");
     fclose(fp);
-
-    printf("Results stored in 'selection_sort_results.txt'\n");
-
     return 0;
 }
